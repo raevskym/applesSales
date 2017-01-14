92d0ea940d06f953a43b68230451e273068fe72a
Michael Raevsky <raevskym@sas.upenn.edu>
2017-01-13 19:40:32 -0700

    added top description

--- ui.R
+++ ui.R
@@ -1,7 +1,22 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Projecting impact of Apple ad expenditures on iMac sales using customer-centric probability models"),
+  titlePanel("Projecting impact of Apple ad expenditures on iMac sales using probability models"),
+  fluidRow(
+    column(12,
+           "This project employs a modelling aproach I picked up while I was", withTags({a(href="https://www.linkedin.com/in/peterfader", "Peter Fader")}), "'s student and research assistant. Fader pioneered an approach for modeling customer retention that is fundamentally different from traditional curve fitting / regression methods. Combining probability theory with non-linear optimization allows us to model customer behavior in a much more nuanced matter. This customer-centric modeling approach is rapidly gaining traction in industry, and is the fundamental building block behind statistical marketting consulting startup", withTags({a(href="http://www.zodiacmetrics.com/", "Zodiac Metrics")}),"."
+    ),
+    column(12, 
+          "
+          "
+           ),
+    column(12, 
+           "
+           "
+    ),
+    column(12,
+           "A full pdf report of this investigation and the R code are both available on my website. This app was built with the Shiny R library."
+          ),
   sidebarLayout(position = "right",
     sidebarPanel(
       radioButtons('modelType', "Choose Individual-level Model", choices = c('Weibull' = 2, 'Exponential' = 1)),
@@ -17,4 +32,4 @@ shinyUI(fluidPage(
       textOutput('description')
       )
   )
-))
+)))
